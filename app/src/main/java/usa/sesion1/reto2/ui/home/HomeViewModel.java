0a85d1d3850c062e2205366dd251be92bb0c6a87
joswald123 <julianaandreaoswald@gmail.com>
2021-11-29 16:38:39 -0800

    First commit

---
+++ app/src/main/java/usa/sesion1/reto2/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package usa.sesion1.reto2.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
